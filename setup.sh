b6ea559524161b05a703cb44ce66e2099e180539
Kevin Miao <kevinmiao@berkeley.edu>
2021-04-08 23:22:33 -0700

    dependencies up

---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+# Create Conda Environment
+
+conda create -n skin python=3.8.3
+
+# Install Packages
+
+pip install sklearn
+pip install numpy
+pip install pandas
+pip install torch
+pip install opencv-python
+pip install scikit-image
