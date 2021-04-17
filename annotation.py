3fda2dceac0ad17201683272f916223ec0ed6da4
Kevin Miao <kevinmiao@berkeley.edu>
2021-04-17 01:07:11 -0700

    updated annotation

--- annotation.py
+++ annotation.py
@@ -66,7 +66,10 @@ def bounding_box(im, contours):
 
 
     # Pull out second largest contour; form a bounding box
-    c = list(sorted_dict.keys())[-2]
+    try:
+        c = list(sorted_dict.keys())[-2]
+    except:
+        return -1, -1, -1, -1
 
     x,y,w,h = cv2.boundingRect(contours[c])
     return x, y, w, h 
@@ -74,28 +77,48 @@ def bounding_box(im, contours):
 
 if __name__ == "__main__":
     working_directory = [x for x in os.listdir() if 'HAM10000_images' in x]
+    print(working_directory)
     
     for directory in working_directory:
         for image_path in os.listdir(directory):
             path = os.path.join(directory, image_path)
-            
             ##########################
-            #START FIXME
+            #METHOD 1
             ##########################
             im = cv2.imread(path, 1)
             imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(imgray, args.kernel, args.sigma)
+            blurred = cv2.GaussianBlur(imgray, (7,7), 5.0)
+            ret, thresh = cv2.threshold(blurred, 127, 255, 0)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            x,y,w,h = bounding_box(blurred, contours)
+            x1 = int(x - pad)
+            y1 = int(y - pad)
+            w1 = int(w + extra)
+            h1 = int(h + extra)
+            ##########################
+            # END METHOD 1
+            ##########################
+            
+            ##########################
+            #METHOD 2
+            ##########################
             high_contrast = skimage.exposure.adjust_log(blurred, args.gamma)
             ret, thresh = cv2.threshold(high_contrast, 127, 255, 0)
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             x,y,w,h = bounding_box(high_contrast, contours)
-            x = int(x - args.xypadding)
-            y = int(y - args.xypadding)
-            w = int(w + args.hwpadding)
-            h = int(h + args.hwpadding)
+            x2 = int(x - args.xypadding)
+            y2 = int(y - args.xypadding)
+            w2 = int(w + args.hwpadding)
+            h2 = int(h + args.hwpadding)
+            ##########################
+            # END METHOD 2
+            ##########################
+            if w1*h1 >= w2*h2:
+                x,y,w,h = x1,y1,w1,h1
+            else:
+                x,y,w,h = x2,y2,w2,h2
+            
             result.append(pd.DataFrame([[path, x, y, w, h]], columns=result.columns),ignore_index=True)
-            ##########################
-            # END FIXME
-            ##########################
+
 
     result.to_csv(args.output)
